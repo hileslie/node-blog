970d43fbfdaaba49f87a03f677bbb8833dbd12a4
chao.zhang <chao.zhang@beibei.com>
2020-02-19 23:32:11 +0800

    get post

---
+++ express-node/routes/blog.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/list', function(req, res, next) {
+  res.json({
+      error: 0,
+      data: [
+          1,
+          2,
+          3,
+      ]
+  })
+});
+
+router.get('/detail', function(req, res, next) {
+  res.json({
+      error: 0,
+      data: {
+          id: 0,
+          title: 'xxx'
+      }
+  })
+});
+
+module.exports = router;
