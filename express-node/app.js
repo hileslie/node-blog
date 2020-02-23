4d7161636933e499c4ac21bfbf63423ad519e39e
chao.zhang <chao.zhang@beibei.com>
2020-02-23 11:21:44 +0800

    express 通 mysql

--- express-node/app.js
+++ express-node/app.js
@@ -4,17 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 const blogRouter = require('./routes/blog');
 const userRouter = require('./routes/user');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,8 +22,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use(function(req, res, next) {
+//   console.log(123)
+//   next()
+// })
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 app.use('/api/blog', blogRouter);
 app.use('/api/user', userRouter);
 
--- express-node/routes/blog.js
+++ express-node/routes/blog.js
@@ -1,26 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const { 
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog,
+} = require('../controller/blog');
+const { SuccessModel, ErrorModel} = require('../model/resModel');
 
 /* GET home page. */
-router.get('/list', function(req, res, next) {
-  res.json({
-      error: 0,
-      data: [
-          1,
-          2,
-          3,
-      ]
-  })
+router.get('/list', function (req, res, next) {
+	// 获取列表
+	let author = req.query.author || '';
+	const keyword = req.query.keyword || '';
+	// if (req.query.isadmin) {
+	// 	const loginCheckResult = loginCheck(req);
+	// 	if (loginCheckResult) {
+	// 		// 未登录
+	// 		return loginCheckResult;
+	// 	}
+	// 	author = req.session.username;
+	// }
+	const result = getList(author, keyword);
+	return result.then(listData => {
+		res.json(new SuccessModel(listData));
+	})
 });
 
-router.get('/detail', function(req, res, next) {
-  res.json({
-      error: 0,
-      data: {
-          id: 0,
-          title: 'xxx'
-      }
-  })
+router.get('/detail', function (req, res, next) {
+	res.json({
+		error: 0,
+		data: {
+			id: 0,
+			title: 'xxx'
+		}
+	})
 });
 
 module.exports = router;
